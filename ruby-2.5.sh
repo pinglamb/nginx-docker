0169d5a96e4819b75d19bd6e310cad8891151291
Yachi Lo <yaachi@gmail.com>
2018-01-04 11:07:06 +0800

    update ruby to 2.5

---
+++ ruby-2.5.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+source /nd_build/buildconfig
+
+RUBY_VERSION=2.5.0
+RUBY_GEM_VERSION=2.6.13
+RUBY_INSTALL_VERSION=0.6.1
+BUNDLER_VERSION=1.16.1
+TZINFO_VERSION=1.2017.2
+
+header "Installing Ruby..."
+
+# skip installing gem documentation
+run mkdir -p /usr/local/etc \
+	&& { \
+		echo 'install: --no-document'; \
+		echo 'update: --no-document'; \
+	} >> /usr/local/etc/gemrc
+
+run curl -L https://github.com/postmodern/ruby-install/archive/v$RUBY_INSTALL_VERSION.tar.gz | tar xzv
+run make -C ruby-install-$RUBY_INSTALL_VERSION install
+run rm -r ruby-install-$RUBY_INSTALL_VERSION
+
+run ruby-install --system ruby $RUBY_VERSION -- --disable-install-doc
+run gem update --system $RUBY_GEM_VERSION
+run gem install bundler -v $BUNDLER_VERSION
+run gem install tzinfo-data -v $TZINFO_VERSION
